2a93fd901e00706470ee444cacee697f55bcc1a3
Rami-bou <bouarour.mohamed2003@gmail.com>
2026-08-13 10:36:47 +0200

    ddd

--- 5Agents_work_on_power_alloc/realistic_fixedP1_Donor_Knapsack.py
+++ 5Agents_work_on_power_alloc/realistic_fixedP1_Donor_Knapsack.py
@@ -651,9 +651,9 @@ def build_prompt(train):
     return prompt_primary
 
 
-data = gen_channels(190)
-train = data[:90]
-test = data[90:110]
+data = gen_channels(200)
+train = data[100]
+test = data[100:]
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 prompt_secondary_allocation = build_prompt(train)
 
